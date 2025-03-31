2927d26fede40dc8eaf4fe21e79d31606e9351fd
nhan <nhoang2929@gmail.com>
2025-03-31 14:47:57 +0700

    feat: add page and websocker for realtime leaderboard

--- src/main/java/com/guess/it/core/config/SecurityConfig.java
+++ src/main/java/com/guess/it/core/config/SecurityConfig.java
@@ -7,7 +7,13 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -18,7 +24,19 @@ public class SecurityConfig {
                         .anyRequest()
                         .authenticated())
                 .httpBasic(Customizer.withDefaults())
+                .cors((cors) -> cors.configurationSource(corsConfigurer()))
                 .csrf(AbstractHttpConfigurer::disable)
                 .build();
+    }
+    @Bean
+    public CorsConfigurationSource corsConfigurer() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedOrigins(List.of("http://127.0.0.1:5500"));
+        corsConfiguration.setAllowedHeaders(List.of("*"));
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
     }
 }
